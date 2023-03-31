195d7b457438deddc547cb3b6fb3d8d84c38d8c0
Yuvateja Reddy <yuvatejareddy003@gmail.com>
2023-03-31 11:15:51 +0530

    Photo Book Program

---
+++ ConsoleApp1TestProject1/TestPhotoBook.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProblemSolving;
+
+namespace TestProject
+{
+    [TestClass]
+    public class PhotoBookTest
+    {
+        [TestMethod]
+        public void TestDefaultPhotoBook()
+        {
+            PhotoBook defaultBook = new PhotoBook();
+            Assert.AreEqual(16,defaultBook.GetNumberPages());
+        }
+
+        [TestMethod]
+        public void TestLargePhotoBook()
+        {
+            AddPhotoBook largeBook = new AddPhotoBook();
+            Assert.AreEqual(64, largeBook.GetNumberPages());
+        }
+        [TestMethod]
+        public void TestMediumPhotoBook()
+        {
+            PhotoBook mediumBook = new PhotoBook(32);
+            Assert.AreEqual(32, mediumBook.GetNumberPages());
+        }
+    }
+}
---
+++ ProblemSolving/PhotoBookProgram.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving
+{
+    public class PhotoBook
+    {
+        public int totalPages;
+        public PhotoBook() {
+            this.totalPages = 16;
+        }
+        public PhotoBook(int totalPages)
+        {
+            this.totalPages = totalPages;
+        }
+        public int GetNumberPages()
+        {
+            return this.totalPages;
+        }
+    }
+
+    public class AddPhotoBook : PhotoBook
+    {
+
+        public AddPhotoBook() : base(64) { }
+    }
+
+    internal class PhotoBookProgram
+    {
+        public static void Main(string[] args)
+        {
+            PhotoBook smBook = new PhotoBook();
+            PhotoBook mdBook = new PhotoBook(32);
+            AddPhotoBook lgBook = new AddPhotoBook();
+            Console.WriteLine($"{smBook.GetNumberPages()} Pages");
+            Console.WriteLine($"{mdBook.GetNumberPages()} Pages");
+            Console.WriteLine($"{lgBook.GetNumberPages()} Pages");
+
+        }
+    }
+}
